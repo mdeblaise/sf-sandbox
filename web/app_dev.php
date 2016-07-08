950bfa999d1815326bdb95ff36fb6ef26659234f
Matelo <m.deblaise@meuhmeuhconcept.com>
2016-07-08 09:44:32 +0200

    Enable docker ip for app.dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,9 +10,15 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+$available_ips = ['127.0.0.1', 'fe80::1', '::1'];
+//Add docker IPS
+for ($i = 1; $i < 254; $i++) {
+   $available_ips[] = "172.17.0.".$i;
+}
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], $available_ips) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
