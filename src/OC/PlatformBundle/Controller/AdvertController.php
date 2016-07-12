1069857118ee0a9d72e2da6aa439760f811d38d6
Matelo <m.deblaise@meuhmeuhconcept.com>
2016-07-12 15:37:59 +0200

    Modif AdvertController->viewAction function

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -11,9 +11,21 @@ use Symfony\Component\HttpFoundation\Response;
 class AdvertController extends Controller
 {
   public function indexAction()
-  {
-    $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'toto'));
-    
-    return new Response($content);
+  { 
+    return new Response('hello world');
   }
+
+  public function viewAction($id)
+  {
+    return new Response('Affichage de la vue n°'.$id.'');
+  }
+
+// On récupère tous les paramètres en arguments de la méthode
+   public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
+    }
 }
